19db1d0af6bb3105f479cb2342a10bf2255e7777
antraxx <hnilsson90@hotmail.com>
2015-08-14 19:56:05 +0200

    Added function for uploading jar file to the server.

--- client/views/addComponentGroup/addComponentGroup.js
+++ client/views/addComponentGroup/addComponentGroup.js
@@ -5,5 +5,25 @@ angular.module('conductorRepository')
                 options: '[\r\t{\r\t\t"name": "",\r\t\t"dataType": ""\r\t}\r]',
                 components: '[\r\t{\r\t\t"name": "",\r\t\t"options": [\r\t\t\t{\r\t\t\t\t"name": "",\r\t\t\t\t"dataType": ""\r\t\t\t}\r\t\t],\r\t\t"properties": {\r\t\t\t"testPropertyName": {\r\t\t\t\t"value": "",\r\t\t\t\t"dataType": "",\r\t\t\t\t"enum": []\r\t\t\t}\r\t\t},\r\t\t"methods": {\r\t\t\t"testMethodName": {\r\t\t\t\t"parameters": {\r\t\t\t\t\t"testParameterName": "integer"\r\t\t\t\t}\r\t\t\t}\r\t\t}\r\t}\r]'
             }
+
+            $scope.jars = $meteor.collection(JarRepository, false, JarRepository);
+            $scope.fileUploads;
+
+            $scope.addJar = function() {
+                $('<input type="file">').bind("change", function(event) {
+                    $scope.jars.save(event.target.files[0]).then(
+                        function(data) {
+                            console.log(data);
+                            if (data == null || data.length == 0) {
+                                alert("File upload failed.");
+                            } else {
+                                $scope.fileUploads = data;
+                            }
+                        },
+                        function(error) {
+                            alert("File upload failed. Error message: " + error);
+                        });
+                }).click();
+            };
         }
     ]);
