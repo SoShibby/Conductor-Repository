1837b2efab3dd5e506f8f40f359d824832bb3141
antraxx <hnilsson90@hotmail.com>
2015-08-18 20:21:28 +0200

    Changed the path where the jar files are stored

--- server/lib/collections/collections.js
+++ server/lib/collections/collections.js
@@ -1,5 +1,5 @@
 JarRepository = new FS.Collection("jarRepository", {
-    stores: [new FS.Store.FileSystem("jars", {path: "C:\\Users\\Henrik\\uploads"})]
+    stores: [new FS.Store.FileSystem("jars", {path: "C:\\uploads"})]
 });
 
 ComponentGroups = new Meteor.Collection("componentGroups");
