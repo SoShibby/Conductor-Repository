fbe9cdd9c00891857198dc90862ad69ae1b43ac7
antraxx <hnilsson90@hotmail.com>
2015-08-20 20:36:38 +0200

    Added ComponentGroups collection to the client

--- client/lib/collection.js
+++ client/lib/collection.js
@@ -1,3 +1,5 @@
 JarRepository = new FS.Collection("jarRepository", {
     stores: [new FS.Store.FileSystem("jars", {path: "/uploads"})]
 });
+
+ComponentGroups = new Mongo.Collection("componentGroups");
