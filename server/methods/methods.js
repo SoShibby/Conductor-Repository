fa127a70709756093809dd4cdeb738e53010c944
antraxx <hnilsson90@hotmail.com>
2015-08-20 20:48:12 +0200

    Added a check so no component group exist with the same org, name and version before we add a new component group

--- server/methods/methods.js
+++ server/methods/methods.js
@@ -6,6 +6,16 @@ Meteor.methods({
 
         ComponentGroupValidator.validate(componentGroup);
 
+        componentGroup.org = User.getUser(this.userId).org;
+
+        var existComponentGroup = ComponentGroups.findOne({
+            org: componentGroup.org,
+            name: componentGroup.name,
+            version: componentGroup.version
+        });
+
+        if (existComponentGroup) {
+            throw new Meteor.Error(400, 'A component group already exist with the same org, name and version.');
         }
 
         componentGroup.org = this.userId;
